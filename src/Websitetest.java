c1f155b1398d8105acdda7b13da17a5a45730bd2
Neveen-1997echo <neveenabujwaid@gmail.com>
2024-10-22 14:21:56 +0300

    update

--- src/Websitetest.java
+++ src/Websitetest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -35,109 +37,109 @@ public class Websitetest {
 		driver.get(MyWebsiteurl);
 
 	}
-//
-//	@Test(priority = 1)
-//
-//	public void testregister() throws InterruptedException {
-//
-//		int Randomindexfirstname = rand.nextInt(FirstNames.length);
-//		int Randomindexlasttname = rand.nextInt(LastNames.length);
-//		String FirstName = FirstNames[Randomindexfirstname];
-//		String LastName = LastNames[Randomindexlasttname];
-//
-//		int RandomIndexemail = rand.nextInt(30);
-//		String Email = FirstName + LastName + RandomIndexemail + DomainName;
-//		String Finalname = FirstName + LastName + RandomIndexemail;
-//
-//		WebElement registerbtn = driver.findElement(By.linkText("Login or register"));
-//		registerbtn.click();
-//
-//		driver.findElement(By.xpath("//*[@id=\"accountFrm\"]/fieldset/button")).click();
-//		WebElement FirstNameinput = driver.findElement(By.id("AccountFrm_firstname"));
-//		FirstNameinput.sendKeys(FirstName);
-//
-//		GlobalUserName = FirstName;
-//
-//		WebElement LastNameinput = driver.findElement(By.id("AccountFrm_lastname"));
-//		LastNameinput.sendKeys(LastName);
-//
-//		WebElement Emailinput = driver.findElement(By.id("AccountFrm_email"));
-//		Emailinput.sendKeys(Email);
-//
-//		WebElement Addressinput = driver.findElement(By.id("AccountFrm_address_1"));
-//		Addressinput.sendKeys("Jordan-Irbid");
-//
-//		WebElement Cityinput = driver.findElement(By.id("AccountFrm_city"));
-//		Cityinput.sendKeys("iRBID");
-//
-//		WebElement Regioninput = driver.findElement(By.id("AccountFrm_zone_id"));
-//		Select selector1 = new Select(Regioninput);
-//		int RandomRegion = rand.nextInt(1, 100);
-//		selector1.selectByIndex(RandomRegion);
-//		Thread.sleep(3000);
-//
-//		WebElement Zipinput = driver.findElement(By.id("AccountFrm_postcode"));
-//		Zipinput.sendKeys("123456");
-//
-//		WebElement Countryinput = driver.findElement(By.id("AccountFrm_country_id"));
-//		Select selector2 = new Select(Countryinput);
-//
-//		int Randomindexcountry = rand.nextInt(0, 8);
-//
-//		WebElement LogiNnameinput = driver.findElement(By.id("AccountFrm_loginname"));
-//		LogiNnameinput.sendKeys(Finalname);
-//
-//		LoginName = Finalname;
-//
-//		WebElement Passwordinput = driver.findElement(By.id("AccountFrm_password"));
-//		Passwordinput.sendKeys(pass);
-//
-//		WebElement Confirmpassinput = driver.findElement(By.id("AccountFrm_confirm"));
-//		Confirmpassinput.sendKeys(pass);
-//
-//		WebElement Checkbox = driver.findElement(By.id("AccountFrm_agree"));
-//		Checkbox.click();
-//
-//		driver.findElement(By.xpath("//*[@id=\"AccountFrm\"]/div[5]/div/div/button")).click();
-//
-//	}
-//
-//	@Test(priority = 2)
-//	public void testlogout() throws InterruptedException {
-//
-//		WebElement Navbar = driver.findElement(By.linkText("Welcome " + "back " + GlobalUserName));
-//
-//		Actions action = new Actions(driver);
-//
-//		action.moveToElement(Navbar).perform();
-//
-//		Thread.sleep(2000);
-//
-//		WebElement logout = driver.findElement(By.linkText("Not " + GlobalUserName + "? Logoff"));
-//		Thread.sleep(2000);
-//		logout.click();
-//		Thread.sleep(2000);
-//
-//	}
-//
-//	@Test(priority = 3)
-//
-//	public void loginwithsameuser() throws InterruptedException {
-//		Thread.sleep(2000);
-//		WebElement Loginbtn = driver.findElement(By.linkText("Login or register"));
-//		Thread.sleep(2000);
-//		Loginbtn.click();
-//
-//		WebElement LogiNnameinput = driver.findElement(By.id("loginFrm_loginname"));
-//		LogiNnameinput.sendKeys(LoginName);
-//
-//		WebElement Passwordinput = driver.findElement(By.id("loginFrm_password"));
-//		Passwordinput.sendKeys(pass);
-//
-//		driver.findElement(By.xpath("//*[@id=\"loginFrm\"]/fieldset/button")).click();
-//		Thread.sleep(2000);
-//
-//	}
+
+	@Test(priority = 1)
+
+	public void testregister() throws InterruptedException {
+
+		int Randomindexfirstname = rand.nextInt(FirstNames.length);
+		int Randomindexlasttname = rand.nextInt(LastNames.length);
+		String FirstName = FirstNames[Randomindexfirstname];
+		String LastName = LastNames[Randomindexlasttname];
+
+		int RandomIndexemail = rand.nextInt(30);
+		String Email = FirstName + LastName + RandomIndexemail + DomainName;
+		String Finalname = FirstName + LastName + RandomIndexemail;
+
+		WebElement registerbtn = driver.findElement(By.linkText("Login or register"));
+		registerbtn.click();
+
+		driver.findElement(By.xpath("//*[@id=\"accountFrm\"]/fieldset/button")).click();
+		WebElement FirstNameinput = driver.findElement(By.id("AccountFrm_firstname"));
+		FirstNameinput.sendKeys(FirstName);
+
+		GlobalUserName = FirstName;
+
+		WebElement LastNameinput = driver.findElement(By.id("AccountFrm_lastname"));
+		LastNameinput.sendKeys(LastName);
+
+		WebElement Emailinput = driver.findElement(By.id("AccountFrm_email"));
+		Emailinput.sendKeys(Email);
+
+		WebElement Addressinput = driver.findElement(By.id("AccountFrm_address_1"));
+		Addressinput.sendKeys("Jordan-Irbid");
+
+		WebElement Cityinput = driver.findElement(By.id("AccountFrm_city"));
+		Cityinput.sendKeys("iRBID");
+
+		WebElement Regioninput = driver.findElement(By.id("AccountFrm_zone_id"));
+		Select selector1 = new Select(Regioninput);
+		int RandomRegion = rand.nextInt(1, 100);
+		selector1.selectByIndex(RandomRegion);
+		Thread.sleep(3000);
+
+		WebElement Zipinput = driver.findElement(By.id("AccountFrm_postcode"));
+		Zipinput.sendKeys("123456");
+
+		WebElement Countryinput = driver.findElement(By.id("AccountFrm_country_id"));
+		Select selector2 = new Select(Countryinput);
+
+		int Randomindexcountry = rand.nextInt(0, 8);
+
+		WebElement LogiNnameinput = driver.findElement(By.id("AccountFrm_loginname"));
+		LogiNnameinput.sendKeys(Finalname);
+
+		LoginName = Finalname;
+
+		WebElement Passwordinput = driver.findElement(By.id("AccountFrm_password"));
+		Passwordinput.sendKeys(pass);
+
+		WebElement Confirmpassinput = driver.findElement(By.id("AccountFrm_confirm"));
+		Confirmpassinput.sendKeys(pass);
+
+		WebElement Checkbox = driver.findElement(By.id("AccountFrm_agree"));
+		Checkbox.click();
+
+		driver.findElement(By.xpath("//*[@id=\"AccountFrm\"]/div[5]/div/div/button")).click();
+
+	}
+
+	@Test(priority = 2)
+	public void testlogout() throws InterruptedException {
+
+		WebElement Navbar = driver.findElement(By.linkText("Welcome " + "back " + GlobalUserName));
+
+		Actions action = new Actions(driver);
+
+		action.moveToElement(Navbar).perform();
+
+		Thread.sleep(2000);
+
+		WebElement logout = driver.findElement(By.linkText("Not " + GlobalUserName + "? Logoff"));
+		Thread.sleep(2000);
+		logout.click();
+		Thread.sleep(2000);
+
+	}
+
+	@Test(priority = 3)
+
+	public void loginwithsameuser() throws InterruptedException {
+		Thread.sleep(2000);
+		WebElement Loginbtn = driver.findElement(By.linkText("Login or register"));
+		Thread.sleep(2000);
+		Loginbtn.click();
+
+		WebElement LogiNnameinput = driver.findElement(By.id("loginFrm_loginname"));
+		LogiNnameinput.sendKeys(LoginName);
+
+		WebElement Passwordinput = driver.findElement(By.id("loginFrm_password"));
+		Passwordinput.sendKeys(pass);
+
+		driver.findElement(By.xpath("//*[@id=\"loginFrm\"]/fieldset/button")).click();
+		Thread.sleep(2000);
+
+	}
 
 	public void AddItemToThecart() throws InterruptedException {
 
